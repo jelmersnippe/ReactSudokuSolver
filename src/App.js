c073ce7a1f55be07417deabfae6c3d09b4129a39
jelmersnippe <jelmersnippe@hotmail.com>
2020-09-19 22:02:25 +0200

    Add functionality to upload sudoku grid in JSON format

--- src/App.js
+++ src/App.js
@@ -3,28 +3,9 @@ import './App.scss';
 import './Sudoku.scss';
 import { solveGrid } from './SudokuLogic';
 
-const sudoku = [
-    [8, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 3, 6, 0, 0, 0, 0, 0],
-    [0, 7, 0, 0, 9, 0, 2, 0, 0],
-    [0, 5, 0, 0, 0, 7, 0, 0, 0],
-    [0, 0, 0, 0, 4, 5, 7, 0, 0],
-    [0, 0, 0, 1, 0, 0, 0, 3, 0],
-    [0, 0, 1, 0, 0, 0, 0, 6, 8],
-    [0, 0, 8, 5, 0, 0, 0, 1, 0],
-    [0, 9, 0, 0, 0, 0, 4, 0, 0],
-];
-
 function App() {
-    const [activeGrid, setActiveGrid] = useState(
-        // We map through and return the sliced rows
-        // This is to clone the base grid without a reference
-        // Otherwise (with a reference), every change made to the activeGrid would
-        // also affect the base grid
-        sudoku.map((row) => {
-            return row.slice();
-        })
-    );
+    const [sudokuToSolve, setSudukoToSolve] = useState(null);
+    const [activeGrid, setActiveGrid] = useState(null);
 
     const renderGrid = (grid) => (
         <div className="sudoku-grid">
@@ -34,7 +15,7 @@ function App() {
                         {row?.map((cell, cellIndex) => (
                             <div
                                 className={`cell ${
-                                    sudoku[rowIndex][cellIndex] !== 0
+                                    sudokuToSolve[rowIndex][cellIndex] !== 0
                                         ? 'pre-filled'
                                         : ''
                                 }`}
@@ -49,29 +30,61 @@ function App() {
         </div>
     );
 
+    const handleFileSelect = (evt) => {
+        var file = evt.target.files[0];
+        var reader = new FileReader();
+        reader.onload = (function () {
+            return function (e) {
+                try {
+                    let json = JSON.parse(e.target.result);
+                    setActiveGrid(json);
+                    setSudukoToSolve(
+                        json.map((row) => {
+                            return row.slice();
+                        })
+                    );
+                } catch (ex) {
+                    alert('ex when trying to parse json = ' + ex);
+                }
+            };
+        })(file);
+        reader.readAsText(file);
+    };
+
     return (
         <div className="App">
-            {activeGrid && (
-                <div className="sudoku-wrapper">{renderGrid(activeGrid)}</div>
+            {activeGrid && sudokuToSolve && (
+                <>
+                    <div className="sudoku-wrapper">
+                        {renderGrid(activeGrid)}
+                    </div>
+                    <button
+                        className="action solve"
+                        onClick={() => {
+                            var startTime = new Date().getTime();
+
+                            // We use the spread operator to create a new array object
+                            // If we change the values but don't actually modify the array object
+                            // React won't fire a proper state update and the
+                            // hooks won't work, nor will the Dom rerender
+                            setActiveGrid([...solveGrid(activeGrid)]);
+
+                            var endTime = new Date().getTime();
+                            console.log(`Solved in ${endTime - startTime} ms`);
+                        }}
+                    >
+                        Solve
+                    </button>
+                </>
             )}
-            <button
-                className="action solve"
-                onClick={() => {
-                    var startTime = new Date().getTime();
-
-                    // We use the spread operator to create a new array object
-                    // If we change the values but don't actually modify the array object
-                    // React won't fire a proper state update and the
-                    // hooks won't work, nor will the Dom rerender
-                    setActiveGrid([...solveGrid(activeGrid)]);
-
-                    var endTime = new Date().getTime();
-
-                    console.log(`Solved in ${endTime - startTime} ms`);
-                }}
-            >
-                Solve
-            </button>
+            <div>
+                <input
+                    type="file"
+                    accept="application/json"
+                    onChange={handleFileSelect}
+                    className="action upload"
+                />
+            </div>
         </div>
     );
 }
